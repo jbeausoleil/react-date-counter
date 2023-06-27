ba4c4b03c0b7a8ac0f182dba4feed01091edd35c
Justin Beausoleil <beausoleil.justin@gmail.com>
2023-06-27 11:46:44 -0600

    Adding text input for counter and conditional rendering for reset.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { useState } from "react";
-import button from 'react-bootstrap/button';
+import button from "react-bootstrap/button";
 
 export default function App() {
   return (
@@ -11,15 +11,13 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const date = new Date() // Today's date
-  const calculatedDate = new Date(date.setDate(date.getDate() + count)).toDateString()
-
-  function handlePreviousStep() {
-    if (step > 0) setStep((s) => s - 1);
-  }
+  const date = new Date(); // Today's date
+  const calculatedDate = new Date(
+    date.setDate(date.getDate() + count)
+  ).toDateString();
 
   function handleReset() {
     setCount(0);
@@ -29,15 +27,35 @@ function Counter() {
   return (
     <>
       <div>
-        <input type="range" min="0" max="10" value={step} onChange={e=> setStep(Number(e.target.value))}/>
-        <span>{step}</span>
+        <p>Step: {step}</p>
+
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
-      <button className="reset" onClick={handleReset}>Reset</button>
+      <div>
+        {count !== 0 || step !== 1 ? (
+          <button className="reset" onClick={handleReset}>
+            Reset
+          </button>
+        ) : null}
+      </div>
+
       <p>
         <span>
           {count === 0
